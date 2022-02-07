ca09afe6bf2d98a3ed47da3ef2d710b947a33159
luicast <luycast@hotmail.com>
2022-02-07 12:48:55 -0500

    configurando el login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,22 +1,34 @@
-import React from 'react';
+import React,{ useRef} from 'react';
 import { Link } from 'react-router-dom';
 import '@styles/login.scss';
 import logo from '@logos/logo_yard_sale.svg';
 
 const Login = () => {
+	const from = useRef(null);
+
+	const handleSubmit = (event) =>{
+		event.preventDefault();
+		const fromData = new FormData(from.current);
+		const data = {
+			username:fromData.get('email'),
+			password: fromData.get('password'),
+		}
+		console.log(data);
+	}
+
 	return (
 		<div className="Login">
 			<div className="Login-container">
 				<img src={logo} alt="logo" className="logo" />
-				<form action="/" className="form">
+				<form action="/" className="form" ref={from}>
 					<label htmlFor="email" className="label">Email address</label>
-					<input type="text" id="email" placeholder="platzi@example.cm" className="input input-email" />
+					<input type="text" name="email" placeholder="platzi@example.cm" className="input input-email" />
 					<label htmlFor="password" className="label">Password</label>
-					<input type="password" id="password" placeholder="*********" className="input input-password" />
-					<input type="submit" value="Log in" className="primary-button login-button" />
+					<input type="password" name="password" placeholder="*********" className="input input-password" />
+					<button className="primary-button login-button"onClick={handleSubmit}> Log in</button>
 					<Link to="/">Forgot my password</Link>
 				</form>
-				<button className="secondary-button signup-button">Sign up</button>
+				<button className="secondary-button signup-button" >Sign up</button>
 			</div>
 		</div>
 	);
